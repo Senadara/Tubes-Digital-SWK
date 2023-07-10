036a214f10f11d7e642b196b597e184b07195c2c
Septian Nanda Saputra <putrasanjaya498@gmail.com>
2023-07-11 01:13:24 +0700

    Upload code for first

---
+++ src/main/java/com/mycompany/tubesdigitalswk/TubesDigitalSWK.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tubesdigitalswk;
+
+/**
+ *
+ * @author putra
+ */
+public class TubesDigitalSWK {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
